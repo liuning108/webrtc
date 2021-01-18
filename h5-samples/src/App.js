9f8f8c5758210d7df8d91c203b9d52f227099738
liuning <122273014@qq.com>
2021-01-18 22:36:44 +0800

    done PeerConnection.jsx

--- h5-samples/src/App.js
+++ h5-samples/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import TakeSnap from "./component/TakeSnap";
 import ScreenSharing from "./component/ScreenSharing";
 import PeerConnection from "./component/PeerConnection";
+import Microphone from "./component/Microphone";
 
 function App() {
   return (
--- h5-samples/src/component/PeerConnection.jsx
+++ h5-samples/src/component/PeerConnection.jsx
@@ -49,7 +49,7 @@ class PeerConnection extends  React.Component {
             console.log(`使用的视频设备为:${videoTracks[0].label}`)
         }
         if(audioTracks.length>0){
-            console.log(`使用的视频设备为:${audioTracks[0].label}`)
+            console.log(`使用的视频设备为:`,audioTracks)
         }
 
        let  configuration={"iceServers":[{"urls":["turn:stun.l.liuningff.xyz"],"username":"liuning","credential":"123456"}],"iceTransportPolicy":"all","iceCandidatePoolSize":"0"}
@@ -58,24 +58,141 @@ class PeerConnection extends  React.Component {
         peerConnA.addEventListener("icecandidate",this.onlceCandidateA)
         peerConnA.addEventListener("iceconnectionstatechange",this.onlceStateChangeA)
 
+
         peerConnB = new RTCPeerConnection(configuration)
+        peerConnB.addEventListener("icecandidate",this.onlceCandidateB)
+        peerConnB.addEventListener("iceconnectionstatechange",this.onlceStateChangeB)
+        peerConnB.addEventListener('track',this.gotRemoteStream)
         console.log("远端的PeerConnection"+peerConnB)
 
-        try {
-        }catch (e){
+        localStream.getTracks().forEach((track)=>{
+            peerConnA.addTrack(track,localStream)
+        })
 
+        try {
+            console.log("peerConnA创建提议Offer开始")
+            const offer = await peerConnA.createOffer()
+            await this.onCreateOffetSuccess(offer)
+        }catch (e){
+            await this.onCreateSessionDescriptionError(e)
+        }
+
+
+    }
+
+    gotRemoteStream=(e)=>{
+        if(remoteVideo.srcObject!==e.streams[0]){
+            remoteVideo.srcObject= e.streams[0]
+            console.log("开始接收远端流")
+        }
+
+        console.log("gotRemoteStream",e)
+    }
+
+    onCreateSessionDescriptionError =(error)=>{
+     console.log(`创建会话描述SD错误:${error.toString()}`)
+    }
+    //创建Offer成功
+    onCreateOffetSuccess=async (desc)=>{
+        console.log(`peerConnA 创建Offer返回SDP信息:${desc.sdp}`)
+        try {
+            await peerConnA.setLocalDescription(desc)
+            this.onSetLocalSuccess(peerConnA)
+
+        }catch (e){
+            this.onSetSessionDescriptionError(e)
+        }
+        console.log("peerConnB开始远端描述")
+        try {
+            await peerConnB.setRemoteDescription(desc)
+            this.onSetRemoteSuccess(peerConnB)
+        }catch (e){
+            this.onSetSessionDescriptionError(e)
+        }
+
+        console.log("peerConnB开始创建应答Answer")
+        try {
+            const answer = await peerConnB.createAnswer()
+            await  this.onCreateAnswerSuccess(answer)
+        }catch (e) {
+            this.onCreateSessionDescriptionError(e)
         }
     }
 
-    onlceStateChangeA(){
-        alert("onlceStateChangeA")
+    onCreateAnswerSuccess=async (answer)=>{
+           console.log('PeerConnB的应答Answer'+answer.sdp)
+           console.log('peerConnB设置本地描述开始setLocalDescription')
+           try {
+               await peerConnB.setLocalDescription(answer)
+               this.onSetLocalSuccess(peerConnB)
+           }catch (e) {
+               this.onSetSessionDescriptionError(e)
+           }
+
+           console.log('peerConnA设置远端描述开始setRemoteDescription')
+           try {
+               await peerConnA.setRemoteDescription(answer)
+               this.onSetRemoteSuccess(peerConnA)
+           } catch (e) {
+               this.onSetSessionDescriptionError(e)
+           }
+
+    }
+
+    onSetLocalSuccess=(pc)=>{
+        console.log(`${this.getName(pc)} setLocalDescription`)
+    }
+    onSetSessionDescriptionError=(error)=>{
+        console.log(`设置描述SD信息错误${error.toString()}`)
+    }
+
+    onSetRemoteSuccess=(pc)=>{
+        console.log(`${this.getName(pc)} onSetRemoteSuccess`)
+    }
+
+    getName = (pc)=>{
+        return (pc===peerConnA)?'peerConnA':'peerConnB'
+    }
+
+    onlceStateChangeA=(event)=>{
+        console.log("onlceStateChangeA",peerConnA.iceConnectionState,event)
+    }
+
+    onlceStateChangeB=(event)=>{
+        console.log("onlceStateChangeA",peerConnB.iceConnectionState,event)
+
     }
     onlceCandidateA = async(event)=>{
         try{
-            alert(event.candidate)
+            if(event.candidate){
+                await peerConnB.addIceCandidate(event.candidate)
+                this.onAddIceCandidateSuccess(peerConnB)
+            }
+        }catch (e){
+        }
+    }
+
+
+    onlceCandidateB = async(event)=>{
+        try{
+            if(event.candidate){
+                await peerConnA.addIceCandidate(event.candidate)
+                this.onAddIceCandidateSuccess(peerConnA)
+            }
         }catch (e){
 
         }
+    }
+
+    onAddIceCandidateSuccess=(pc)=>{
+        console.log(`${this.getName(pc)} 添加IceCandi成功`)
+    }
+
+    hangup=()=>{
+        peerConnA.close()
+        peerConnB.close()
+        peerConnA=null
+        peerConnB=null
     }
 
 
@@ -85,11 +202,14 @@ class PeerConnection extends  React.Component {
                 <h1>
                     <span>RTCPeerConnection Demo</span>
                 </h1>
+
+                <h2>本地</h2>
                 <video ref={this.localVideoRef} playsInline autoPlay muted/>
-                <video ref={this.remoteVideoRef}  playsInline autoPlay muted/>
+                <h2>远端</h2>
+                <video ref={this.remoteVideoRef}  playsInline autoPlay/>
                 <Button onClick={this.start}>开始</Button>
                 <Button onClick={this.call}>呼叫</Button>
-                <Button>挂断</Button>
+                <Button onClick={this.hangup}>挂断</Button>
 
             </div>
         )
